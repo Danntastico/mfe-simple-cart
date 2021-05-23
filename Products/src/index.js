79e2459527b18c797377fba302748ddd80aaf401
Danilo <nicolasdpf@gmail.com>
2021-05-23 08:37:12 -0500

    setting Products bootstrap so it could load deps async

--- Products/src/index.js
+++ Products/src/index.js
@@ -1,10 +1,2 @@
-import faker from 'faker'
-
-let products = ''
-
-for (let i = 0; i < Math.floor(10 + Math.random()*20); i++) {
-  const name = faker.commerce.productName()
-  products += `<div>${name}</div>` 
-}
-
-document.getElementById('dev-products').innerHTML = products
+//This gives posibility of loading the project dependencies asynchronously.
+import('./bootstrap')
