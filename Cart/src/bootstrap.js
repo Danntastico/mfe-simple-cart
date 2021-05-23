7a1e593876850550234c15c90045f9e970d95500
Danilo <nicolasdpf@gmail.com>
2021-05-23 09:23:38 -0500

    Setting up Cart's mount fnc
    so you can import and use it into the Container

--- Cart/src/bootstrap.js
+++ Cart/src/bootstrap.js
@@ -1,7 +1,17 @@
 import faker from 'faker'
 
-const cartText = `<div>You have ${faker.random.number()} items in your cart. </div>`
+const mount = (el) => {
+  const cartText = `<div>You have ${faker.random.number()} items in your cart. </div>`
 
-document.getElementById('cart-dev').innerHTML = cartText;
+  el.innerHTML = cartText;
+}
 
-console.log('Cart is available')
+if(process.env.NODE_ENV === 'development'){
+  const el = document.getElementById('cart-dev')
+
+  if(el){
+    mount(el)
+  }
+}
+
+export { mount }
